59b082cb47bc9b5e09973081cb05b99b65518353
Kori-San <aymen@ezzayer.net>
2022-05-25 16:24:21 +0200

    Add seumtus.sh and add verbosity to code

--- seumtus.sh
+++ seumtus.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-## VARS
-### Paths
+# [Vars]
+# -> Getting the path of the script and then getting the directory of the script.
 path=$(readlink -f "$0") && path=$(dirname "${path}")
 words_path="${path}/data/word_list" 
-### Random VARS
+# -> Getting a random line from the word list and then converting it to lowercase.
 line_number=$(shuf -i 1-"$(wc -l "${words_path}" | cut -d ' ' -f1)" -n 1)
 word=$( sed -n "${line_number}p" "$words_path" ) && word="${word,,}"
-### Attempts VARS
+# -> Setting the attempts to 0 and the max attempts to 10.
 attempts="0"
 max_attempts="10"
-### User VARS
-#### Create ${user_best}
+# -> Creating a string with the same length as the word, but with the first letter and the rest of the letters replaced by @.
 for (( i = 0; i < ${#word}; i++ )); do
     letter="${word:$i:1}"
     if [ ${i} -eq 0 ]; then
@@ -21,7 +20,8 @@ for (( i = 0; i < ${#word}; i++ )); do
     fi
 done
 
-## Functions
+# [Functions]
+# -> It's replacing the @ by the letter if the letter is correct.
 update_best(){
     for (( i = 0; i <= ${#word}; i++ )); do
         letter="${word:$i:1}"
@@ -32,13 +32,14 @@ update_best(){
     done
 }
 
+# -> It's printing the word with the correct letters in green and the wrong letters in red.
 pretty_printd(){
     for (( i = 0; i < ${#word}; i++ )); do
-        #### Letters
+        # ~> It's getting the letter from the word, the letter from the user_word and the letter from the user_best.
         letter="${word:$i:1}"
         user_letter="${user_word:$i:1}"
         user_better="${user_best:$i:1}"
-        #### Prints
+        # ~> It's printing the word with the correct letters in green and the wrong letters in red.
         if [ "${user_better,,}" = "${letter,,}" ] ; then
             tput setab 2
             printf "%s" "${letter^^}"
@@ -48,18 +49,22 @@ pretty_printd(){
         else
             printf "*"
         fi
-        #### Clean
+        # ~> It's resetting the color.
         tput sgr0
     done
+    # ~> It's printing a carriage return.
     printf "\r"
 }
 
-# Pretty_printf() - Final Pretty print | Show if letter is correct / wrong placed or incorrect
+# -> It's printing the word with the correct letters in green and the wrong letters in red.
 pretty_printf(){
+    # ~> It's printing a carriage return.
     printf "\r"
     for (( i = 0; i < "${#word}"; i++ )); do
+        # ~> It's getting the letter from the word, the letter from the user_word and the letter from the user_best.
         letter="${word:$i:1}"
         user_letter="${user_word:$i:1}"
+        # ~> It's printing the word with the correct letters in green and the wrong letters in red.
         if [ "${i}" -eq "0" ] || [ "${user_letter,,}" = "${letter,,}" ] ; then
             tput setab 2
             printf "%s" "${letter^^}"
@@ -70,29 +75,33 @@ pretty_printf(){
             tput setab 1
             printf "%s" "${user_letter^^}"
         fi
+        # ~> It's resetting the color.
         tput sgr0
     done
 }
-## Script
+
+# [Main]
+# -> It's a loop that will run until the user_best is equal to the word or the attempts are greater than the max_attempts.
 while : ; do
-    ### Updating ${user_best}
+    # ~> It's replacing the @ by the letter if the letter is correct.
     update_best
-    ### Check if WIN / FAILED
+    # ~> It's checking if the user_best is equal to the word or if the attempts are greater than the max_attempts.
     if [ "${user_best,,}" = "${word,,}" ] || [ "${attempts}" -gt "${max_attempts}" ]; then
         break
     fi
-    ### Attempts count and display
+    # ~> It's printing the number of attempts left, printing the word with the correct letters in green and 
+    #    the wrong letters in red, reading the user input and then printing the word with the correct letters
+    #    in green and the wrong letters in red.
     attempts=$(( attempts + 1 ))
-    printf "You have %d attempts left\n" "$(( 10 - attempts ))"
-    ### Pretty print of word
+    printf "You have %d attempts left\n" "$(( 11 - attempts ))"
     pretty_printd
-    # Read user entry
     read -r user_word > /dev/null
-    # Re-Print
     pretty_printf
-    # Finition
     printf "\n\n"
 done
+
+# -> It's printing the word and then it's printing congratulations
+#    if the user_best is equal to the word or try again if it's not.
 printf "The word was %s, " "${word^^}"
 if [ "${user_best,,}" = "${word,,}" ] ; then
     printf "congratulations !\n"
